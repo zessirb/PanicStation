fa86a416ed820826e8450df1a0b84fea85ef26b2
Zessirb <zessirb@gmail.com>
2018-05-14 17:32:26 +0200

    Instantiation d'une connection mysql

--- api/controllers/emotionAction.js
+++ api/controllers/emotionAction.js
@@ -1,9 +1,30 @@
 'use strict';
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password"
+});
+
+
 exports.add_emotion = function(req, res) {
-    res.json("{'action': 'test'}");
+    connection.connect(function (error) {
+        if (error) {
+            res.json("{'error': 'Cannot connect to mysql server.'}");
+            return;
+        }
+        res.json("{'action': 'test'}");
+    });
 };
 
 exports.get_emotion = function(req, res) {
-    res.json("{'action': 'test'}");
+    connection.connect(function (error) {
+        if (error) {
+            res.json("{'error': 'Cannot connect to mysql server.'}");
+            return;
+        }
+        res.json("{'action': 'test'}");
+    });
 };
