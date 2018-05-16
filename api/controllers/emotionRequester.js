c524ad54bd47835e3c9fc18ae14c0c092887440f
Zessirb <zessirb@gmail.com>
2018-05-16 16:03:09 +0200

    Ajout d'une valeur totale représent la quantité d'émotion

--- api/controllers/emotionRequester.js
+++ api/controllers/emotionRequester.js
@@ -31,6 +31,32 @@ exports.get_emotion = function(req, res) {
     });
 };
 
+exports.get_terminal_emotion = function(req, res) {
+    var terminalId = req.params.terminalId;
+    var connection = get_mysql_connection();
+    connection.connect(function(error) {
+        if (error) {
+            res.json("{'error': 'Cannot connect to mysql server.'}");
+            return;
+        }
+        var sql = "SELECT * FROM emotion WHERE terminal = ? ORDER BY time DESC;";
+        connection.query(sql, [terminalId], function(error, result) {
+            if (error) {
+                res.json("{'error': 'Cannot request mysql server.'}");
+                return;
+            }
+            var i = 0;
+            var total = 0;
+            while (i < result.length && i < 5) {
+                var emotion = result[0];
+                total += (1 - i*0.2) * (emotion["anger"] + emotion["disgust"] + emotion["fear"] + emotion["sadness"] + emotion["surprise"]);
+                i++;
+            }
+            res.json(total);
+        });
+    })
+};
+
 exports.add_emotion = function(req, res) {
     var terminal = req.body.terminal;
     var anger = req.body.anger;
--- api/routes/emotionRoute.js
+++ api/routes/emotionRoute.js
@@ -1,13 +1,14 @@
 'use strict';
 module.exports = function(app) {
-    var emotionAction = require('../controllers/emotionAction');
+    var emotionRequester = require('../controllers/emotionRequester');
 
     app.route('/emotion')
-        .get(emotionAction.get_emotion)
-        .post(emotionAction.add_emotion);
+        .get(emotionRequester.get_emotion)
+        .post(emotionRequester.add_emotion);
+    app.route('/emotion/:terminalId').get(emotionRequester.get_terminal_emotion);
 
     function remove_old_emotions() {
-        emotionAction.clear_emotions();
+        emotionRequester.clear_emotions();
         setTimeout(remove_old_emotions, 10000);
     }
     remove_old_emotions();
