411bb28e6d772f0b5f72d9e424078429af51bee2
Raiponz <spooki.leserpent@orange.fr>
2018-05-17 14:26:10 +0200

    add deltatime to module

--- client/module/borderify.js
+++ client/module/borderify.js
@@ -3,14 +3,17 @@ var hostname = window.location.hostname;
 //var subsite = hostname.split(".").first();
 console.log("subsite : " + window.location.hostname.split(".")[0])
 var elementClickCounter = []
+var defaultTime = Date.now()
 document.onclick = function(event){
 	console.log(event.target.nodeName);
+	var itemName = event.target.nodeName;
 	if (event.target.nodeName == "INPUT" || event.target.nodeName == "BUTTON") {
-		console.log(event.target.type)
+		itemName += "_" + event.target.type
 	}
+	var newTime = Date.now();
+	var deltaTime = newTime - defaultTime;
+
+	console.log(deltaTime)
+	defaultTime = Date.now()
+
 };
-
-
-function incrementCounter(element){
-
-}
